ce6250726153d66b99f16625e04b7fbe239df472
Andrew Sutton <andrewsydsutton@gmail.com>
2021-09-09 15:28:40 -0500

    Changes on 9/9/21

---
+++ index.js
@@ -0,0 +1,16 @@
+document.getElementById('secondloginBtn').addEventListener('click', (e) => {
+    document.getElementById('firstLoginBtn').classList.add('smallText')
+    document.getElementById('firstLoginBtn').textContent = "hello again, user1234!"
+})
+
+document.getElementById('saveArtist').addEventListener('click', ()=> {
+    if(document.getElementById('saveArtist').textContent === "save artist"){
+        document.getElementById('saveArtist').classList.remove('btn-outline-danger')
+        document.getElementById('saveArtist').classList.add('btn-success') 
+        document.getElementById('saveArtist').textContent= "saved"
+    } else {
+        document.getElementById('saveArtist').classList.add('btn-outline-danger')
+        document.getElementById('saveArtist').classList.remove('btn-success') 
+        document.getElementById('saveArtist').textContent= "save artist"
+    }
+})
